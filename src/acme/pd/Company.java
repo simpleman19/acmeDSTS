773f1d03661214dad4af27b436d9f4752e4a026a
paresa <paresa@paresas-mbp.attlocal.net>
2018-06-10 14:12:56 -0500

    adding Company pd and ui updates

--- src/acme/pd/Company.java
+++ src/acme/pd/Company.java
@@ -4,8 +4,21 @@ import java.io.File;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.UUID;
-
-public class Company {
+//////////GUI/////////
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+///////////////////////
+public class Company 
+{
     private UUID id;
     private String name = "ACME";
     private Map map;
@@ -19,114 +32,162 @@ public class Company {
     private int latenessMarginMinutes = 5;
     private double blocksPerMile = 5.2;
     private double courierMilesPerHour = 5.8;
-
-    public Company() {
+    
+    public Company()
+    {      
         // TODO initialize company
         File file = new File("null");
         this.map = new Map(file);
-    }
 
-    public UUID getId() {
+        couriers = new HashMap<UUID, Courier>();
+        tickets = new HashMap<UUID, Ticket>();
+        customer = new HashMap<UUID, Customer>();
+        currentUser = new User();
+    }
+    
+
+    public UUID getId()
+    {
         // TODO fix with database
-        if (this.id == null) {
+        if (this.id == null)
+        {
             this.id = UUID.randomUUID();
         }
         return id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public Map getMap() {
+    public Map getMap()
+    {
         return map;
     }
 
-    public void setMap(Map map) {
+    public void setMap(Map map)
+    {
         this.map = map;
     }
 
-    public HashMap<UUID, Courier> getCouriers() {
+    public HashMap<UUID, Courier> getCouriers()
+    {
         return couriers;
     }
 
-    public void setCouriers(HashMap<UUID, Courier> couriers) {
+    public void setCouriers(HashMap<UUID, Courier> couriers)
+    {
         this.couriers = couriers;
     }
 
-    public HashMap<UUID, Ticket> getTickets() {
+    public HashMap<UUID, Ticket> getTickets()
+    {
         return tickets;
     }
 
-    public void setTickets(HashMap<UUID, Ticket> tickets) {
+    public void setTickets(HashMap<UUID, Ticket> tickets)
+    {
         this.tickets = tickets;
     }
 
-    public HashMap<UUID, Customer> getCustomer() {
+    public HashMap<UUID, Customer> getCustomer()
+    {
         return customer;
     }
 
-    public void setCustomer(HashMap<UUID, Customer> customer) {
+    public void setCustomer(HashMap<UUID, Customer> customer)
+    {
         this.customer = customer;
     }
 
-    public User getCurrentUser() {
+    public User getCurrentUser()
+    {
         return currentUser;
     }
 
-    public void setCurrentUser(User currentUser) {
+    public void setCurrentUser(User currentUser)
+    {
         this.currentUser = currentUser;
     }
 
-    public BigDecimal getBonus() {
+    public BigDecimal getBonus()
+    {
         return bonus;
     }
 
-    public void setBonus(BigDecimal bonus) {
+    public void setBonus(BigDecimal bonus)
+    {
         this.bonus = bonus;
     }
 
-    public BigDecimal getFlatBillingRate() {
+    public BigDecimal getFlatBillingRate()
+    {
         return flatBillingRate;
     }
 
-    public void setFlatBillingRate(BigDecimal flatBillingRate) {
+    public void setFlatBillingRate(BigDecimal flatBillingRate)
+    {
         this.flatBillingRate = flatBillingRate;
     }
 
-    public BigDecimal getBlockBillingRate() {
+    public BigDecimal getBlockBillingRate()
+    {
         return blockBillingRate;
     }
 
-    public void setBlockBillingRate(BigDecimal blockBillingRate) {
+    public void setBlockBillingRate(BigDecimal blockBillingRate)
+    {
         this.blockBillingRate = blockBillingRate;
     }
 
-    public int getLatenessMarginMinutes() {
+    public int getLatenessMarginMinutes()
+    {
         return latenessMarginMinutes;
     }
 
-    public void setLatenessMarginMinutes(int latenessMarginMinutes) {
+    public void setLatenessMarginMinutes(int latenessMarginMinutes)
+    {
         this.latenessMarginMinutes = latenessMarginMinutes;
     }
 
-    public double getBlocksPerMile() {
+    public double getBlocksPerMile()
+    {
         return blocksPerMile;
     }
 
-    public void setBlocksPerMile(double blocksPerMile) {
+    public void setBlocksPerMile(double blocksPerMile)
+    {
         this.blocksPerMile = blocksPerMile;
     }
 
-    public double getCourierMilesPerHour() {
+    public double getCourierMilesPerHour()
+    {
         return courierMilesPerHour;
     }
 
-    public void setCourierMilesPerHour(double courierMilesPerHour) {
+    public void setCourierMilesPerHour(double courierMilesPerHour)
+    {
         this.courierMilesPerHour = courierMilesPerHour;
     }
+    
+    public static Company getDefaultAcme() {
+    	Company acme = new Company();
+    	acme.setName("Acme");
+    	acme.setBonus(acme.bonus);
+    	acme.setCourierMilesPerHour(15);
+    	acme.setBlocksPerMile(5.5);
+    	acme.setLatenessMarginMinutes(2);
+    	acme.setFlatBillingRate(acme.flatBillingRate);
+    	acme.setBlockBillingRate(acme.blockBillingRate);
+    	
+    	return acme; 
+    }
+   
+    
 }
