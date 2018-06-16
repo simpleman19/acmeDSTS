ad32924b7453af887434ef715ca5ce2598ee1fe6
DeeDee <DeeDee@10.30.95.18>
2018-06-16 14:56:42 -0500

    Added comments and cleaned up some stuff.

--- src/acme/ui/MapUI.java
+++ src/acme/ui/MapUI.java
@@ -34,27 +34,33 @@ public class MapUI extends AcmeBaseJPanel {
   }
   
   public void buildPanel() {
-    // Initialize your panel here
-    // repaint after changes
-    
-
    
+    //get the map  from company
     Map map = getCompany().getMap();
+    
+    
     this.removeAll();
     this.setLayout(null);
     
+    //title label
     JLabel lblMap = new JLabel("Map");
     lblMap.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
     lblMap.setBounds(27, 34, 71, 27);
     add(lblMap);
     
+    //these will hold the buttons (intersections) and labels for the map
     int size = map.getMap().length;
     JButton mapButtons[][] = new JButton[size][size];
     JLabel mapLablesNS[] = new JLabel[size];
     JLabel mapLablesEW[] = new JLabel[size];
     
+    //used for positioning of the buttons and labels 
     int yPos = 100;
-    int y = 0;
+    int xPos = 35;
+  
+    
+    //these are unicodes to make the arrows for the labels
+    //to show which direction the road is 
     char downArrow = '\u2193';
     char upArrow = '\u2191';
     char leftArrow = '\u2190';
@@ -62,8 +68,10 @@ public class MapUI extends AcmeBaseJPanel {
     char biDirLRArrow = '\u2194';
     char biDirUDArrow = '\u2195';
     
+    //this draws the labels for the east and west roads
     for(int x = 0; x < size; x++)
     {
+      //figures out which arrow to use
       if((map.getMap()[x][0].getEWroad().isBidirectional()))
       {
         mapLablesEW[x]= new JLabel(map.getMap()[x][0].getEWroad().getName() + " " + biDirLRArrow); 
@@ -76,24 +84,30 @@ public class MapUI extends AcmeBaseJPanel {
       {
         mapLablesEW[x]= new JLabel(map.getMap()[x][0].getEWroad().getName() + " " + leftArrow); 
       }
+      
       mapLablesEW[x].setFont(new Font("Lucida Grande", Font.PLAIN, 16));
       mapLablesEW[x].setBounds(30, yPos, 40, 20);
       mapLablesEW[x].setHorizontalAlignment(SwingConstants.LEFT);
       add(mapLablesEW[x]);
       
+      //keep track of positioning
       yPos=yPos+55;
     }
     
-    int xPos = 35;
+    //reset the y position for the rest of the map
     yPos=35;
    
     
+    //this draws the buttons (intersections) and labels for the north and sout roads
     for(int x = 0; x < size; x++)
     {
+      //configure positioning
       xPos=xPos+55;
       yPos = 35;
       
-      if(map.getMap()[0][x].getNSroad().isBidirectional())
+      //this is the drawing for the labels for the north and south road
+      //it was convenient to just do it in this loop
+      if(map.getMap()[0][x].getNSroad().isBidirectional())          //this is figuring out which arrow to use
       {
         mapLablesNS[x]= new JLabel("<html>" + map.getMap()[0][x].getNSroad().getName() + "<br>" + biDirUDArrow + "</html>");
       }
@@ -113,13 +127,17 @@ public class MapUI extends AcmeBaseJPanel {
       
 
       
-      for(y = 0; y < size; y++)
+      for(int y = 0; y < size; y++)
       {
         yPos = yPos+55;
         
+        //make an intersection 
+        //you'll notice I flipped the x and the y here for the map.getMap()
+        //this is drawing the map one col at a time
         mapButtons[x][y] = new JButton(map.getMap()[y][x].getIntersectionName());
         mapButtons[x][y].setBounds(xPos, yPos, 50, 50);
         
+        //color code the intersections 
         if(map.getMap()[y][x].isClosed(LocalDate.now()))
         {
           mapButtons[x][y].setBackground(Color.RED);
@@ -131,22 +149,25 @@ public class MapUI extends AcmeBaseJPanel {
           mapButtons[x][y].setBackground(Color.YELLOW);
           mapButtons[x][y].setOpaque(true);
         }
+        
+        //handle clicking of intersection 
         mapButtons[x][y].addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
             intersectionClicked(((JButton) e.getSource()).getText());
           }
         });
+        //add to the screen
         add(mapButtons[x][y]);
       }
-      
-      
     }
     
+    //this is for athestic purposes 
     ScrollPane scrollPane = new ScrollPane();
     scrollPane.setBackground(new Color(93, 184, 202));
     scrollPane.setBounds(75, 75, size*59, size*59);
     add(scrollPane);
     
+    //now draw the legends 
     xPos=xPos+95;
     yPos = 35;
     JLabel lblLegend = new JLabel("Legend");
@@ -176,6 +197,9 @@ public class MapUI extends AcmeBaseJPanel {
     add(homeButton);
     yPos = yPos + 55;
     
+    
+    this.revalidate();
+    this.repaint();
   }
   
   public void intersectionClicked(String intersectionName)
