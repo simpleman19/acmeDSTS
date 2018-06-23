b00b1f7fc3b3ee81989306bba025a9dcb2db5cff
jake6642 <37449431+jake6642@users.noreply.github.com>
2018-06-23 05:15:57 -0500

    The blocks are still off sometimes

--- src/acme/pd/Path.java
+++ src/acme/pd/Path.java
@@ -14,7 +14,7 @@ public class Path {
     private int blocksBetweenHomeandPickup;
     private int blocksBetweenPickupandDropoff;
     private int blocksBetweenDropoffandHome;
-
+    private static String PICKUP = "Pickup", DELIVER = "Deliver", NONE = "";
 
     public ArrayList<MapIntersection> getPath() {
         return path;
@@ -23,21 +23,21 @@ public class Path {
     public void setPath(ArrayList<MapIntersection> path) {
         this.path = path;
     }
-    
+
     public int getBlocksBetweenHomeandDropoff() {
-      return blocksBetweenHomeandDropoff;
+        return blocksBetweenHomeandDropoff;
     }
 
     public void setBlocksBetweenHomeandDropoff(int blocksBetweenHomeandDropoff) {
-      this.blocksBetweenHomeandDropoff = blocksBetweenHomeandDropoff;
+        this.blocksBetweenHomeandDropoff = blocksBetweenHomeandDropoff;
     }
-    
+
     public int getBlocksBetweenHomeandPickup() {
-      return blocksBetweenHomeandPickup;
+        return blocksBetweenHomeandPickup;
     }
 
     public void setBlocksBetweenHomeandPickup(int blocksBetweenHomeandPickup) {
-      this.blocksBetweenHomeandPickup = blocksBetweenHomeandPickup;
+        this.blocksBetweenHomeandPickup = blocksBetweenHomeandPickup;
     }
 
     public int getBlocksBetweenPickupandDropoff() {
@@ -47,51 +47,88 @@ public class Path {
     public void setBlocksBetweenPickupandDropoff(int blocksBetweenPickupandDropoff) {
         this.blocksBetweenPickupandDropoff = blocksBetweenPickupandDropoff;
     }
-    
+
     public int getBlocksBetweenDropoffandHome() {
-      return blocksBetweenDropoffandHome;
+        return blocksBetweenDropoffandHome;
     }
 
-  public void setBlocksBetweenDropoffandHome(int blocksBetweenDropoffandHome) {
-      this.blocksBetweenDropoffandHome = blocksBetweenDropoffandHome;
-  }
-   
+    public void setBlocksBetweenDropoffandHome(int blocksBetweenDropoffandHome) {
+        this.blocksBetweenDropoffandHome = blocksBetweenDropoffandHome;
+    }
 
     public ArrayList<String> getDeliveryInstructions(Company company) {
         ArrayList<String> instructions = new ArrayList<>();
+        MapIntersection pickUp = null;
+        MapIntersection dropOff = null;
         if (this.path != null && this.path.size() > 0) {
             Direction lastDir = company.getMap().getTravelDirection(this.path.get(0), this.path.get(1));
             Direction dir = null;
+            int step = 0, blocks = 0;
+            String interaction = NONE;
             for (int i = 1; i < this.path.size() - 1; i++) {
-                dir = company.getMap().getTravelDirection(this.path.get(i), this.path.get(i+1));
+                pickUp = this.path.get(i);
+                step++;
+                dir = company.getMap().getTravelDirection(this.path.get(i), this.path.get(i + 1));
+                step = checkForInteraction(i, step, instructions);
                 while (dir.toString().equalsIgnoreCase(lastDir.toString()) && i < this.path.size() - 2) {
                     i++;
-                    dir = company.getMap().getTravelDirection(this.path.get(i), this.path.get(i+1));
+                    step = checkForInteraction(i, step,  instructions);
+                    dir = company.getMap().getTravelDirection(this.path.get(i), this.path.get(i + 1));
                 }
+                dropOff = this.path.get(i);
                 Road road = null;
+                String roadName = null;
                 if (lastDir == Direction.EAST || lastDir == Direction.WEST) {
                     road = this.path.get(i).getEWroad();
+                    roadName = road.getName() + " Street";
                 } else {
                     road = this.path.get(i).getNSroad();
+                    roadName = "Avenue " + road.getName();
                 }
-                instructions.add("Go " + lastDir.toString() + " on " + road.getName() + " to "
-                        + this.path.get(i).getIntersectionName());
+
+                Path p = company.getMap().getPath(pickUp, dropOff);
+                blocks = p.getBlocksBetweenPickupandDropoff();
+                instructions.add("Step " + step + ": Go " + lastDir.toString() + " on " + roadName + " for "
+                        + blocks + (blocks > 1 ? " Blocks" : " Block")+" to " + this.path.get(i).getIntersectionName());
+
                 lastDir = dir;
+                pickUp = this.path.get(i + 1);
             }
             Road road = null;
+            String roadName = null;
             if (lastDir == Direction.EAST || lastDir == Direction.WEST) {
                 road = this.path.get(this.path.size() - 1).getEWroad();
+                roadName = "Street " + road.getName();
             } else {
                 road = this.path.get(this.path.size() - 1).getNSroad();
+                roadName = "Avenue " + road.getName();
             }
-            instructions.add("Go " + dir.toString() + " on " + road.getName() + " to "
-                    + this.path.get(this.path.size() - 1).getIntersectionName());
-            instructions.add("You have arrived at " + this.path.get(this.path.size() - 1).getIntersectionName());
+            dropOff = this.path.get(this.path.size() - 1);
+            Path p = company.getMap().getPath(pickUp, dropOff);
+            blocks = p.getBlocksBetweenPickupandDropoff();
+            step++;
+            instructions.add("Step " + step + ": Go " + lastDir.toString() + " on " + road.getName() + " for "
+                    + blocks + (blocks > 1 ? " Blocks" : " Block")+" to " + this.path.get(this.path.size() -1).getIntersectionName());
+            step++;
+            instructions.add("Step " + step + ": Confirm ticket completion with " + company.getName());
         }
         return instructions;
     }
 
-    public static void main(String [] args) {
+    private int checkForInteraction(int count, int step, ArrayList<String> instructions) {
+        if (count == this.getBlocksBetweenHomeandPickup()) {
+            instructions.add("Step " + step + ": " + PICKUP + " Package at the intersection of "
+                    + this.path.get(count).getIntersectionName());
+        } else if (count == this.getBlocksBetweenPickupandDropoff()+this.getBlocksBetweenHomeandPickup()) {
+            instructions.add("Step " + step + ": " + DELIVER + " Package at the intersection of "
+                    + this.path.get(count).getIntersectionName());
+        }else {
+            return step;
+        }
+        return step+1;
+    }
+
+    public static void main(String[] args) {
         AcmeUI acme = new AcmeUI();
         for (Ticket t : acme.getCompany().getTickets().values()) {
             if (t.getDeliveryTime() != null) {
